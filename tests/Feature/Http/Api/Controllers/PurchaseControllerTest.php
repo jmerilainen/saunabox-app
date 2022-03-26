921117364b5479ad43c86847f564731034fd277a
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-03-26 17:46:34 +0200

    Add fixed hours for test

--- tests/Feature/Http/Api/Controllers/PurchaseControllerTest.php
+++ tests/Feature/Http/Api/Controllers/PurchaseControllerTest.php
@@ -6,7 +6,11 @@ test('user purchase a slot', function () {
     testTime()->freeze('2022-03-21 10:24:56');
 
     $sauna = sauna()->create();
-    opening()->for($sauna)->create();
+    opening()->for($sauna)->create([
+        'hours' => [
+            'monday' => ['09:00-12:00', '13:00-18:00'],
+        ],
+    ]);
 
     $this
         ->post(route('api.purchase'), [
