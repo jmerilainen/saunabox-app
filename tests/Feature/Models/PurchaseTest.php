2410c09e8de3259cd6144c9b4f18c8b30d6918ad
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-03-26 12:44:44 +0200

    Test Purchase's relations on create

--- tests/Feature/Models/PurchaseTest.php
+++ tests/Feature/Models/PurchaseTest.php
@@ -6,11 +6,24 @@ use App\Models\Purchase;
 use App\Models\Slot;
 use App\Models\User;
 
-it('can create a purcahse model', function() {
+it('can create a purchase model', function() {
     $purchase = Purchase::factory()->create();
 
     expect($purchase)->toBeInstanceOf(Purchase::class);
     expect($purchase->user)->toBeInstanceOf(User::class);
     expect($purchase->slot)->toBeInstanceOf(Slot::class);
     expect($purchase->status)->toBe('pending');
+});
+
+test('that user can make purchase a slot', function () {
+    $user = user()->create();
+    $slot = slot()->create();
+
+    $purchase = Purchase::factory()
+        ->for($user)
+        ->for($slot)
+        ->create();
+
+    expect($user->id)->toBe($purchase->user->id);
+    expect($slot->id)->toBe($purchase->slot->id);
 });
