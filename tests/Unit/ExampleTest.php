591ddf545821ae9eb2c2d76397459f4b37691eb7
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-03-26 09:51:49 +0200

    Init pestphp

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,6 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_true_is_true()
-    {
-        $this->assertTrue(true);
-    }
-}
+it('will test', function() {
+    expect(true)->toBe(true);
+});
